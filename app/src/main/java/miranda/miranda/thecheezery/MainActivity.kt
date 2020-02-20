b684d124532ff2d8d621ef20fa4e9d2ed3960fc6
miranda miranda sanchez <miranda.miranda@potros.itson.edu.mx>
2020-02-20 12:23:55 -0700

    Version 1

---
+++ app/src/main/java/miranda/miranda/thecheezery/MainActivity.kt
@@ -0,0 +1,18 @@
+package miranda.miranda.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_registrar.setOnClickListener(){
+            setContentView(R.layout.activity_registro)
+        }
+
+    }
+}
